d6779f7f07fcc11c53b882e84802d27b9df69129
sanjaysi@usc.edu <sanjaysi@usc.edu>
2016-07-27 01:22:38 -0700

    Added new wrapper for extarcting address from file, string with or without type

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ Input: python input_file_name.json Keyword
 
 example: python address_extractor.py address_extraction_data.json extraction_keyowords.json
 
+
+Using Separate modules to extract address from string with or without keywords and from file.
+
+Example:
+1)
+from address_extractor import getAddressFromStringType
+
+getAddressFromStringType("click here. BIG BOOTY !!!!mis head dr ! Sutphin Boulevard! supthin incall queens.!!!!! - 25",keywords=["avenue","blvd","boulevard","pkwy","parkway","st","street","rd","road","way","drive","lane","alley","ave"])
+
+2)
+from address_extractor import getAddressFromString
+
+getAddressFromString("click here. BIG BOOTY !!!!mis head dr ! Sutphin Boulevard! supthin incall queens.!!!!! - 25")
+
+3)
+from address_extractor import getAddressFromFile
+
+getAddressFromFile("address_extraction_data.json",keywords=["avenue","blvd","boulevard","pkwy","parkway","st","street","rd","road","way","drive","lane","alley","ave"])
+
+
--- address_extractor.py
+++ address_extractor.py
@@ -3,9 +3,11 @@ import sys
 from sets import Set
 
 
-"""def cleanAddresses(input_address_data):
-	for each_addres in input_address_data:"""
-		
+"""
+Keywords: contains list of all the addresses type that we want to use for processing.
+"""
+
+keywords=["avenue","blvd","boulevard","pkwy","parkway","st","street","rd","road","way","drive","lane","alley","ave"]		
 
 def getNum(text,start,dist):
 	end=start+1
@@ -43,9 +45,9 @@ def extractAddress(text,type1,addresses):
 		end=text.lower().find(" "+type1)+len(type1)+1
 	if end !=-1:
 		flag=1
-		flag,bkStart=getNum(text,end-5,50)		
+		flag,bkStart=getNum(text,end-(len(type1)+1),50)		
 		if flag==0:
-			start=getSpace(text,end-5)
+			start=getSpace(text,end-(len(type1)+1))
 		elif flag==1:
 			flag,start=getNum(text,bkStart-1,10)
 			#print (flag, text[start:end])
@@ -54,9 +56,7 @@ def extractAddress(text,type1,addresses):
 		flag,newEnd=getNumNext(text,end,25)
 		if flag:
 			end=newEnd		
-		#print "Input: ", text
-		#print "Extracted: ",text[start:end].replace("\n","")
-		addresses.add(text[start:end].lower().replace("\n",""))
+		addresses.add(text[start:end].lower().replace("\n",""))		
 		if text[end:].lower().find(" ave")!=-1:
 			addresses=extractAddress(text[end:],type1,addresses)
 		return addresses
@@ -64,28 +64,67 @@ def extractAddress(text,type1,addresses):
 #extractAddress("<BR />=?=?=  Table Shower available  =?=?=<br><br>?=Let us make you stress free one day at a time=?<br><br>=?= PUENTE Spa=?= <br><br>TEL:  626-338-8809  <br><br>  1832 Puente Ave. Baldwin Park, CA. 91706  <br><br>Clean Shower Included With Session<br><br>we always hiring beautiful ladies<br><br> Open- 9:00 AM to 9:30 PM<br>	</div>")		
 
 
-def processFile(input_file_name,type1,finalAddressData):
+def processFile(input_file_name,keywords,finalAddressData):
 	input_data=""
 	with open(input_file_name) as data_file:
 		input_data=json.load(data_file)	
 	extracted_address=[]
-	if type1 in input_data:
-		for each_a in input_data[type1]:
+	for each_type in input_data:
+		for each_a in input_data[each_type]:
 			temp={}
 			temp["input"]=each_a
 			addresses=Set()
-			addresses=extractAddress(each_a,type1,addresses)			
+			for each_keyword in keywords:
+				if each_a.lower().find(each_keyword.lower()) > -1:
+					addresses=extractAddress(each_a,each_keyword,addresses)			
 			temp["address"]=list(addresses)		
 			finalAddressData.append(temp)
 
+"""
+Input: Text String
+Output: Json object containing input text string with list of associated present addresses
+
+Uses default keywords embedded in script file
+"""
+def getAddressFromString(text_string):
+	temp={}
+	temp["input"]=text_string
+	addresses=Set()
+	for each_keyword in keywords:		
+		if text_string.lower().find(each_keyword.lower()) > -1:
+			extractAddress(text_string,each_keyword,addresses)
+	temp["address"]=list(addresses)		
+	return temp
+
+"""
+Input: Text String and keyword python list ex: ["ave","street"] etc.
+Output: Json object containing input text string with list of associated present addresses
+
+Uses keywords list passed as an parameter
+"""
+def getAddressFromStringType(text_string,keywords):
+	temp={}
+	temp["input"]=text_string
+	addresses=Set()
+	for each_keyword in keywords:	
+		if text_string.lower().find(each_keyword.lower()) > -1:			
+			extractAddress(text_string,each_keyword,addresses)
+	temp["address"]=list(addresses)		
+	return temp	
+
+"""
+Input: File name and keyword python list ex: ["ave","street"] etc.
+Output: Json object containing list of all input text strings with list of associated present addresses
+"""
+def getAddressFromFile(file_name,keywords):
+	finalAddressData=[]	
+	processFile(file_name,keywords,finalAddressData)
+	return finalAddressData
 
 if __name__ == '__main__':
-	keywords=""
-	with open(sys.argv[2]) as f:
-		keywords=json.load(f)
 	finalAddressData=[]	
-	for each_keyword in keywords:
-		processFile(sys.argv[1],each_keyword,finalAddressData)
+	processFile(sys.argv[1],keywords,finalAddressData)
 	outputFile=open(sys.argv[1].split(".")[0]+"_out.txt","w")
-	json.dump(finalAddressData,outputFile,sort_keys=False,indent=2)
+	json.dump(finalAddressData,outputFile,sort_keys=False,indent=2)	
 	outputFile.close()
+	print ("Output File: ",sys.argv[1].split(".")[0]+"_out.txt")
